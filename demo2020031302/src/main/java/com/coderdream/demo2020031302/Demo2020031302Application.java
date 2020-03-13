84ae3a3cda2f30c083bb72acc21c63b48823d1c8
xulin28709 <xulin28709@billjc.com>
2020-03-13 11:05:33 +0800

    init 202003131105

---
+++ demo2020031302/src/main/java/com/coderdream/demo2020031302/Demo2020031302Application.java
@@ -0,0 +1,13 @@
+package com.coderdream.demo2020031302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2020031302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2020031302Application.class, args);
+    }
+
+}
---
+++ demo2020031302/src/test/java/com/coderdream/demo2020031302/Demo2020031302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderdream.demo2020031302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2020031302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
